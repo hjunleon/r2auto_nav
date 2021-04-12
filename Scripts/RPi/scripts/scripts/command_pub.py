adc090249aff72cc792425976a553cfa29cdeacb
Koh Yang Kai <koh.yangkai@gmail.com>
2021-04-12 19:47:50 +0800

    updates

--- Scripts/RPi/scripts/scripts/command_pub.py
+++ Scripts/RPi/scripts/scripts/command_pub.py
@@ -11,7 +11,7 @@ Copy from r2auto_nav/Target Detection/command_pub2.py in git
 # ROS imports
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int8MultiArray, Float64MultiArray
+from std_msgs.msg import Float32MultiArray, Float64MultiArray
 import numpy as np
 
 # temperature threshold
@@ -26,14 +26,14 @@ class CommandNode(Node):
 
     def __init__(self):
         super().__init__('com_node')
-        self.publisher_ = self.create_publisher(Int8MultiArray, 'com_node', 10)
+        self.publisher_ = self.create_publisher(Float32MultiArray, 'com_node', 10)
         self.heat_subscription = self.create_subscription(Float64MultiArray, 'heat_array', self.callback, 10)
         self.heat_subscription  # prevent unused variable warning
 
     def callback(self, heat_array):
         coordinates = get_bright_loc(heat_array.data)
         self.get_logger().info(f'Coordinates: X:{coordinates[0]}, Y:{coordinates[1]}')
-        comm_array = Int8MultiArray()
+        comm_array = Float32MultiArray()
         comm_array.data = command(coordinates)
         self.publisher_.publish(comm_array)
         self.get_logger().info(
