99313ee50ca6cb13325b4359577cad2723bdc261
Koh Yang Kai <koh.yangkai@gmail.com>
2021-04-12 22:40:47 +0800

    updates

--- Scripts/Workstation/scripts/scripts/command_test_pub.py
+++ Scripts/Workstation/scripts/scripts/command_test_pub.py
@@ -1,8 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Int8MultiArray
-import keyboard
-
 
 class CommandNode(Node):
 
@@ -12,23 +10,12 @@ class CommandNode(Node):
         self.array = Int8MultiArray()
 
     def readKey(self):
-        command = [0, 0, 1]
+        command = [20.0, 10.0, 1.0]
         try:
             while True:
-                print('W A S D for movement')
-                key = keyboard.read_key()
-                if key == 'w':
-                    command[1] = 1
-                if key == 'a':
-                    command[0] = -1
-                if key == 's':
-                    command[1] = -1
-                if key == 'd':
-                    command[0] = 1
-
+                _ = input('Test: [20.0, 10.0, 1.0]\nEnter to continue...')
                 self.array.data = command
                 self.publisher_.publish(self.array)
-                command = [0, 0, 1]
         except Exception as e:
             print(e)
 
@@ -39,10 +26,8 @@ class CommandNode(Node):
 def main(args=None):
     rclpy.init(args=args)
     command_node = CommandNode()
-    command_node.readKey()
 
     command_node.destroy_node()
-
     rclpy.shutdown()
 
 
