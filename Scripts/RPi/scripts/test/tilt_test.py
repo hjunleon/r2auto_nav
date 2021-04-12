7849cdb85bb436c58f99ef630b90d7f94a5f3bd1
Koh Yang Kai <koh.yangkai@gmail.com>
2021-04-12 11:49:34 +0800

    updates

--- Scripts/RPi/scripts/test/tilt_test.py
+++ Scripts/RPi/scripts/test/tilt_test.py
@@ -1,35 +1,41 @@
-#Created on: 070421
-#Author: jasshanK
-#Description: Testing tilt mechanism of turret
+# Created on: 070421
+# Author: jasshanK
+# Description: Testing tilt mechanism of turret
 
-import RPi.GPIO as GPIO
+import pigpio
 from time import sleep
 
-Tilt_PWM = 18
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(Tilt_PWM, GPIO.OUT)
 
-tilt = GPIO.PWM(Tilt_PWM, 50) #servo pwm pin at 50hz
-tilt.start(7.9)
+Tilt_PWM = 18
+pi = pigpio.pi()
+pi.set_mode(Tilt_PWM, pigpio.OUTPUT)
+
+# tilt = GPIO.PWM(Tilt_PWM, 50)  # servo pwm pin at 50hz
+# tilt.start(7.9)
+
+# Enable/Disable Exceptions:
+pigpio.exceptions = True
+
+print('Starting at: ', pi.set_servo_pulsewidth(Tilt_PWM, 1500))
+sleep(0.25)
 
 
 def rotate(angle):
-    angle = float(angle)
-    duty = angle / 18 + 2.5
+    # angle = float(angle)
+    # pulse_width = 1500 + ((angle - 90) * 10)
 
-    if 97 < angle < 113:
-        tilt.ChangeDutyCycle(duty)
-        sleep(1)
-    else:
-        print("Angle not withing range, please try again\n")
+    print("setting to: ", pi.set_servo_pulsewidth(Tilt_PWM, angle))
+    sleep(1)
+    print("set to: ", pi.get_servo_pulsewidth(Tilt_PWM))
+    sleep(1)
+
 
 try:
     while True:
-        angle = input("Input angle between 98 and 112: \n")
-        GPIO.output(Tilt_PWM, True)
+        angle = input("Range: 1650 and 1850\n Input pulse width: \n")
         rotate(angle)
 except Exception as e:
     print(e)
-    GPIO.cleanup()
+    pi.stop()
 finally:
-    GPIO.cleanup()
+    pi.stop()
