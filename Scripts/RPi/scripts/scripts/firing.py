e18ac2d696e500f9f101c880c8e0d3a512d2c12d
Jasshan Kumeresh <jasshank@gmail.com>
2021-04-13 00:06:32 +0800

    w

--- Scripts/RPi/scripts/scripts/firing.py
+++ Scripts/RPi/scripts/scripts/firing.py
@@ -26,10 +26,6 @@ Loading_PWM = 12  # Loading servo pwm pin
 M1 = 26  # firing motors pin
 M_PWM = 13  # bottom firing motor pin 2
 
-# global constants
-complete = 0  # turns to 1 when firing is complete, controlled by a timer
-done = 'not done'  # turns to 1 when actuation is reset
-
 # setting up pins
 pi = pigpio.pi()
 pi.set_mode(DIR, pigpio.OUTPUT)
@@ -47,122 +43,13 @@ pi.set_servo_pulsewidth(Loading_PWM, 700)  # forward: 700, backwards: 1800
 pi.set_PWM_dutycycle(M_PWM, 0)
 pi.write(STEPPER_EN, 1)
 
-# com_array = [0,0] #x and y coordinate, negative: left, down, positive: right, up
-def move_x(array):
-    sleep(1)  # pseudo debouncing
-
-    step_limit = 100  # max number of steps
-    teeth_scale = 2
-    yaw = 0  # keep track of relative position of top layer\
-    cw = 0  # clockwise
-    ccw = 1  # counter clockwise
-    delay = 3000 * (10 ** -6)
-    pi.write(STEPPER_EN, 0) #start stepper
-    if step_limit > yaw > -step_limit and complete == 0:
-        if array[0] < 0:
-            pi.write(DIR, cw)
-            print("Pan left\n")
-            for i in range(int(array[0] // 1.8)*teeth_scale):
-                print("turning left")
-                pi.write(STEP, 1)
-                sleep(delay)
-                pi.write(STEP, 0)
-                sleep(delay)
-                yaw -= 1
-        elif array[0] > 0:
-            pi.write(DIR, ccw)
-            print("Pan right\n")
-            for i in range(int(array[0] // 1.8)*teeth_scale):
-                print("turning right")
-                pi.write(STEP, 1)
-                sleep(delay)
-                pi.write(STEP, 0)
-                sleep(delay)
-                yaw += 1
-    # in case yaw exceeds recommended range
-    elif -step_limit > yaw > step_limit and complete == 0:
-        print("Exceeded pan range, returning to origin\n")
-        while yaw != 0:
-            # barrel on the right
-            if yaw > 0:
-                pi.write(DIR, cw)
-                yaw -= 1
-            # barrel on the left
-            elif yaw < 0:
-                pi.write(DIR, ccw)
-                yaw += 1
-            pi.write(STEP, 1)
-            sleep(delay)
-            pi.write(STEP, 0)
-            sleep(delay)
-
-    # when shooting is complete, move everything back to origin
-    if complete == 1:
-        print("Shooting complete, pan returning to origin\n")
-        if yaw > 0:
-            pi.write(DIR, cw)
-            yaw -= 1
-        elif yaw < 0:
-            pi.write(DIR, ccw)
-            yaw += 1
-        for steps in range(yaw):
-            pi.write(STEP, 1)
-            sleep(delay)
-            pi.write(STEP, 0)
-            sleep(delay)
-
-    # turn off stepper
-    if yaw == 0 and complete == 1:
-        pi.write(STEPPER_EN, 1)
-        done = 'done'
-
-
-def move_y(array):
-    sleep(1)  # pseudo debouncing
-
-    flat_angle = 1850
-    pulse = flat_angle - 10 * (array[1])  # formula to translate angle to pulse width
-
-    if complete == 0:
-        pi.set_servo_pulsewidth(Tilt_PWM, int(pulse))
-        sleep(0.5)
-
-    if complete == 1:
-        pi.set_servo_pulsewidth(Tilt_PWM, flat_angle)
-        sleep(0.5)
-
-
-# power on dc motors when target is sighted, stop powering when target has been shot
-def fire(array):
-    if array[0] == 0 and array[1] == 0 and array[2] == 1 and complete == 0:  # 1 for target found
-        pi.write(M1, 1)
-        pi.set_PWM_dutycycle(13, 180)  # motor on
-
-    if complete == 1:
-        print("Firing complete, motors whining down\n")
-        pi.write(M1, 0)
-        pi.set_PWM_dutycycle(13, 0)  # motor off
-
-
-# loading of balls using servo motor
-def load(array):
-    # needs to be stopping at ball at neutral(?)<---confirm this with rest
-    # servo arm goes back and moves forward in a certain timing range to push balls forward
-    if array[0] == 0 and array[1] == 0 and array[2] == 1 and complete == 0:
-        for i in range(4):
-            print("Loading ball\n")
-            pi.set_servo_pulsewidth(Loading_PWM, 1800)
-            sleep(0.5)
-            pi.set_servo_pulsewidth(Loading_PWM, 700)
-            sleep(0.5)
-            if i == 3:
-                global complete
-                complete = 1
-
-
 class FiringSys(Node):
     def __init__(self):
         super().__init__('firing_mechanism')
+        # constants to mark completion
+        self.complete = 0 #firing complete
+        self.done = 'not done' #firing complete AND returned to origin
+
         # sub 1 for x axis
         self.subscription = self.create_subscription(
             Float32MultiArray,  # topic
@@ -198,6 +85,116 @@ class FiringSys(Node):
         fire(com_array.data)
         load(com_array.data)
 
+    # com_array = [0,0] #x and y coordinate, negative: left, down, positive: right, up
+    def move_x(self, array):
+        sleep(1)  # pseudo debouncing
+
+        step_limit = 100  # max number of steps
+        teeth_scale = 2
+        yaw = 0  # keep track of relative position of top layer\
+        cw = 0  # clockwise
+        ccw = 1  # counter clockwise
+        delay = 3000 * (10 ** -6)
+        pi.write(STEPPER_EN, 0)  # start stepper
+        if step_limit > yaw > -step_limit and self.complete == 0:
+            if array[0] < 0:
+                pi.write(DIR, cw)
+                print("Pan left\n")
+                for i in range(int(array[0] // 1.8) * teeth_scale):
+                    print("turning left")
+                    pi.write(STEP, 1)
+                    sleep(delay)
+                    pi.write(STEP, 0)
+                    sleep(delay)
+                    yaw -= 1
+            elif array[0] > 0:
+                pi.write(DIR, ccw)
+                print("Pan right\n")
+                for i in range(int(array[0] // 1.8) * teeth_scale):
+                    print("turning right")
+                    pi.write(STEP, 1)
+                    sleep(delay)
+                    pi.write(STEP, 0)
+                    sleep(delay)
+                    yaw += 1
+        # in case yaw exceeds recommended range
+        elif -step_limit > yaw > step_limit and self.complete == 0:
+            print("Exceeded pan range, returning to origin\n")
+            while yaw != 0:
+                # barrel on the right
+                if yaw > 0:
+                    pi.write(DIR, cw)
+                    yaw -= 1
+                # barrel on the left
+                elif yaw < 0:
+                    pi.write(DIR, ccw)
+                    yaw += 1
+                pi.write(STEP, 1)
+                sleep(delay)
+                pi.write(STEP, 0)
+                sleep(delay)
+
+        # when shooting is complete, move everything back to origin
+        if self.complete == 1:
+            print("Shooting complete, pan returning to origin\n")
+            if yaw > 0:
+                pi.write(DIR, cw)
+                yaw -= 1
+            elif yaw < 0:
+                pi.write(DIR, ccw)
+                yaw += 1
+            for steps in range(yaw):
+                pi.write(STEP, 1)
+                sleep(delay)
+                pi.write(STEP, 0)
+                sleep(delay)
+
+        # turn off stepper
+        if yaw == 0 and self.complete == 1:
+            pi.write(STEPPER_EN, 1)
+            self.done = 'done'
+
+    def move_y(self, array):
+        sleep(1)  # pseudo debouncing
+
+        flat_angle = 1850
+        pulse = flat_angle - 10 * (array[1])  # formula to translate angle to pulse width
+
+        if self.complete == 0:
+            pi.set_servo_pulsewidth(Tilt_PWM, int(pulse))
+            sleep(0.5)
+
+        if self.complete == 1:
+            pi.set_servo_pulsewidth(Tilt_PWM, flat_angle)
+            sleep(0.5)
+
+    # power on dc motors when target is sighted, stop powering when target has been shot
+    def fire(self, array):
+        if array[0] == 0 and array[1] == 0 and array[2] == 1 and self.complete == 0:  # 1 for target found
+            cur_pulse = pi.get_servo_pulsewidth(Tilt_PWM)
+            pi.set_servo_pulsewidth(Tilt_PWM, cur_pulse)
+            pi.write(M1, 1)
+            pi.set_PWM_dutycycle(M_PWM, 180)  # motor on
+
+        if self.complete == 1:
+            print("Firing complete, motors whining down\n")
+            pi.write(M1, 0)
+            pi.set_PWM_dutycycle(M_PWM, 0)  # motor off
+
+    # loading of balls using servo motor
+    def load(self, array):
+        # needs to be stopping at ball at neutral(?)<---confirm this with rest
+        # servo arm goes back and moves forward in a certain timing range to push balls forward
+        if array[0] == 0 and array[1] == 0 and array[2] == 1 and self.complete == 0:
+            for i in range(4):
+                print("Loading ball\n")
+                pi.set_servo_pulsewidth(Loading_PWM, 1800)
+                sleep(0.5)
+                pi.set_servo_pulsewidth(Loading_PWM, 700)
+                sleep(0.5)
+                if i == 3:
+                    self.complete = 1
+
 def main(args=None):
     print("Actuation initialised\n")
     rclpy.init(args=args)
@@ -219,17 +216,17 @@ if __name__ == '__main__':
         pi.stop()
 
 ### TRASH ###
-# if 40 seconds have passed, complete  = 1
+# if 40 seconds have passed, self.complete  = 1
 # def timer(array):
-#     if array[2] == 1 and complete == 0:
+#     if array[2] == 1 and self.complete == 0:
 #         start_time = time.time()
 #         while (time.time() - start_time) < timer_time:
 #             return
-#         complete = 1
-#     return complete\
+#         self.complete = 1
+#     return self.complete\
 
 # def move_y(array):
-#     global complete
+#     global self.complete
 #     angle = 97.2  # from 97.2 to 112.5 degrees
 #     prev_angle = 97.2
 #     duty = angle / 18 + 2.5  # duty cycle
@@ -251,9 +248,9 @@ if __name__ == '__main__':
 #
 #     prev_angle = angle  # check whats the prev angle
 #
-#     # if complete, move back to origin
-#     if complete == 1:
-#         print("Firing complete, tilt return to origin\n")
+#     # if self.complete, move back to origin
+#     if self.complete == 1:
+#         print("Firing self.complete, tilt return to origin\n")
 #         tilt.ChangeDutyCycle(7.9)
 
 # move_x(com_array.data)
@@ -280,3 +277,7 @@ if __name__ == '__main__':
 # timer_period = 0.2  # seconds
         # self.timer_x = self.create_timer(timer_period, self.callback_x)
         # self.timer_y = self.create_timer(timer_period, self.callback_y)
+
+# # global constants
+# self.complete = 0  # turns to 1 when firing is self.complete, controlled by loading of balls
+# done = 'not done'  # turns to 1 when actuation is reset
