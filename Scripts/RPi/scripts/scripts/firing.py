2d6fcb7e83fb508f81250cce488a8a98f2152d6f
Jasshan Kumeresh <jasshank@gmail.com>
2021-04-13 14:48:27 +0800

    w

--- Scripts/RPi/scripts/scripts/firing.py
+++ Scripts/RPi/scripts/scripts/firing.py
@@ -48,7 +48,7 @@ class FiringSys(Node):
         self.stepper_done = 0  # stepper complete
         self.dc_done = 0  # firing complete
         self.loading_done = 0
-        self.done = 'not done'  # firing complete AND returned to origin
+        self.done = 'searching'  # firing complete AND returned to origin
         self.yaw = 0  # keep track of relative position of top layer
         self.prev_pulse = 1650
 
@@ -174,7 +174,7 @@ class FiringSys(Node):
         # turn off stepper
         if self.yaw == 0 and self.dc_done == 1:
             pi.write(STEPPER_EN, 1)
-            self.done = 'done'
+            self.done = 'annihilated'
 
     def move_y(self, array):
         sleep(1)  # pseudo debouncing
@@ -191,10 +191,8 @@ class FiringSys(Node):
             self.prev_pulse = pulse
             sleep(0.5)
 
-        # if self.dc_done == 1 and self.stepper_done == 1:
-        #     pi.set_servo_pulsewidth(Tilt_PWM, max_angle)
-        #     self.servo_done = 1
-        #     sleep(0.5)
+        if array[2] == 1:
+            self.done = "target found"
 
     # power on dc motors when target is sighted, stop powering when target has been shot
     def fire(self, array):
