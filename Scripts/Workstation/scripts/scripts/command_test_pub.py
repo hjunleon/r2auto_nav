12dae776f5c8595a9b3d61d4d1fadaf113f914f4
Koh Yang Kai <koh.yangkai@gmail.com>
2021-04-13 18:00:14 +0800

    updates

--- Scripts/Workstation/scripts/scripts/command_test_pub.py
+++ Scripts/Workstation/scripts/scripts/command_test_pub.py
@@ -1,7 +1,6 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
-import keyboard
 
 
 class CommandNode(Node):
@@ -9,26 +8,16 @@ class CommandNode(Node):
     def __init__(self):
         super().__init__('com_node')
         self.publisher_ = self.create_publisher(Float32MultiArray, 'com_node', 10)
-        self.array = Int8MultiArray()
+        self.array = Float32MultiArray()
 
     def readKey(self):
-        command = [0.0, 0.0, 1.0]
         try:
             while True:
-                print('W A S D for movement')
-                key = keyboard.read_key()
-                if key == 'w':
-                    command[1] = -10.0
-                if key == 'a':
-                    command[0] = -10.0
-                if key == 's':
-                    command[1] = 10.0
-                if key == 'd':
-                    command[0] = 10.0
+                _ = input('enter')
+                command = [20.0, 10.0, 1.0]
 
                 self.array.data = command
                 self.publisher_.publish(self.array)
-                command = [0.0, 0.0, 1.0]
         except Exception as e:
             print(e)
 
