df19428a9c5ffc3a2ed12c1ae7b0f0c03619d250
Jasshan Kumeresh <jasshank@gmail.com>
2021-04-13 13:32:16 +0800

    w

--- Scripts/RPi/scripts/scripts/firing.py
+++ Scripts/RPi/scripts/scripts/firing.py
@@ -180,7 +180,8 @@ class FiringSys(Node):
 
         flat_angle = 1850
         max_angle = 1510
-        pulse = flat_angle - abs(10 * (array[1])) # formula to translate angle to pulse width
+        if array[1] != 0:
+            pulse = flat_angle - abs(10 * (array[1])) # formula to translate angle to pulse width
 
         if self.dc_done == 0 and self.servo_done == 0:
             pi.set_servo_pulsewidth(Tilt_PWM, int(pulse))
