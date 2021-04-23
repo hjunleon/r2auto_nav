3931f31ca64afcdaebf293e0f13fc7f98108a5d8
Koh Yang Kai <koh.yangkai@gmail.com>
2021-04-23 12:26:21 +0800

    cleanup

--- tb_rpi.sh
+++ tb_rpi.sh
@@ -2,5 +2,6 @@ cd turtlebot3_ws/src/
 rm -r scripts
 svn export https://github.com/hjunleon/r2auto_nav.git/trunk/Scripts/RPi/scripts
 cd ..
-colcon build --packages-select scripts
+colcon build --symlink-install
+source ~/.bashrc
 source install/setup.bash
--- tb_ws.sh
+++ tb_ws.sh
@@ -2,5 +2,6 @@ cd colcon_ws/src/
 rm -r scripts
 svn export https://github.com/hjunleon/r2auto_nav.git/trunk/Scripts/Workstation/scripts
 cd colcon_ws/
-colcon build --packages-select scripts
+colcon build --symlink-install
+source ~/.bashrc
 source install/setup.bash
